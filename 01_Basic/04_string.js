d47680cf82de0654d20cdf1fdc2e59abeeb57013
Prathamesh <prathameshgurav250706@gmail.com>
2026-04-29 12:44:33 +0530

    string function updated

--- 01_Basic/04_string.js
+++ 01_Basic/04_string.js
@@ -1,10 +1,14 @@
+////*****************String************************ */
+
 // const name="abc"
 // const repocount=100
 
-//// console.log(name+repocount)     ////outdated way of concatenating string and number
+// // console.log(name+repocount)     ////outdated way of concatenating string and number
 // console.log(`My name is ${name} and I have ${repocount} repo`)  
 
-const gameName= new String("gtajkkhgkj5")
+// //-------------------------------------------------------------------
+
+// const gameName= new String("gtajkkhgkj5")
 
 // console.log(gameName[0])
 // console.log(gameName.__proto__)
@@ -16,5 +20,24 @@ const gameName= new String("gtajkkhgkj5")
 
 // console.log(gameName.substring(0,3))
 
-s=gameName.slice(0,3)
-console.log(s)
+// s=gameName.slice(-11,5)
+// console.log(s)
+
+////--------------------------------------------------
+
+// const newStringOne="    prathamesh      "
+// console.log(newStringOne)
+// console.log(newStringOne.trim())                    //remove space
+
+////-------------------------------------------------------------
+
+// const text="abcdefgh"
+// console.log(text.replace('abc','---'))
+
+// console.log(text.includes('def'))
+// console.log(text.includes('pqr'))
+
+////-------------------------------------
+
+const name="prathamesh gurav 1234"
+console.log(name.split(" "))
