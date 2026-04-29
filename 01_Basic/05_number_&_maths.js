d38934c874b908facf19f560c1bff02c01c1f2b3
Prathamesh <prathameshgurav250706@gmail.com>
2026-04-29 13:01:32 +0530

    Add number and math examples to 05_number_&_maths.js

---
+++ 01_Basic/05_number_&_maths.js
@@ -0,0 +1,26 @@
+//**************************Number************** */
+
+// const score=100
+// const balance = new Number(100)
+
+// console.log(score)              //100
+// console.log(balance)            //[Number: 100]
+
+// console.log(balance.toString().length)
+// console.log(balance.toFixed(3))
+
+//---------
+
+// const number1= 250706.281105
+// console.log(number1.toPrecision(8))
+
+//-----------
+
+// const num1=10000000000000
+// console.log(num1.toLocaleString())                      //10,000,000,000,000
+// console.log(num1.toLocaleString('en-In'))              //1,00,00,00,00,00,000
+
+
+////*********************MAths******************* */
+
+console.log(Math)
